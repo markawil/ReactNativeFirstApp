8a6884f50870d1bd05ca41b58534eb1c0b8177d0
mark wilkinson <markawil@yahoo.com>
2018-02-08 17:45:54 -0600

    fixed login screen, actually launches now

--- FirstApp/app/screens/Login.js
+++ FirstApp/app/screens/Login.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Alert } from "react-native"
+import { StyleSheet, Text, View, Button, TextInput, Alert } from "react-native";
 
 export default class Login extends Component {
     render() {
-        return {
-            <View style={flex 1, flexDirection: "column"}}>
+        return (
+            <View>
                 <Text>Username:</Text>
                 <TextInput/>
                 <Text>Password:</Text>
                 <TextInput/>
                 <Button title="Login"/>
             </View>
-        }
+        );
     }
 }
