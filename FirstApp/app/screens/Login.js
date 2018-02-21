d1802b200ca6d15951d7b3ac9757b98bafafc5e4
mark wilkinson <markawil@yahoo.com>
2018-02-20 20:37:00 -0600

    using setState now to show a success message

--- FirstApp/app/screens/Login.js
+++ FirstApp/app/screens/Login.js
@@ -4,18 +4,25 @@ import { StyleSheet, Text, View, Button, TextInput, Alert } from "react-native";
 import { StackNavigator } from 'react-navigation';
 
 export default class Login extends Component {    
+    
+    constructor(props) {
+        super(props);
+        this.state = { username: '', password: ''};
+    }
 
     render() {
         const { navigate } = this.props.navigation;
         return (
-            <View style={{flex: 1, flexDirection: "column", backgroundColor: "#B8EDB1"}}>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: "column", backgroundColor: "#B8EDB1"}}>
                 <View style={{flex: 0.3}} />
                 <Text>Username:</Text>
-                <TextInput/>
+                <TextInput style={{width: 200, height: 30, backgroundColor: 'white'}}
+                    onChangeText={(username) => this.setState({username})}/>
                 <Text>Password:</Text>
-                <TextInput/>
+                <TextInput style={{width: 200, height: 30, backgroundColor: 'white'}}
+                    onChangeText={(password) => this.setState({password})}/>
                 <View style={{flex: 0.2}} />
-                <Button title="Login" onPress={() => navigate('MainView', { loginName: "Mark"}) }/>
+                <Button title="Login" onPress={() => navigate('MainView', { loginName: this.state.username}) }/>
             </View>
         );
     }
--- FirstApp/app/screens/MainView.js
+++ FirstApp/app/screens/MainView.js
@@ -3,14 +3,27 @@ import { StyleSheet, Text, View, Button, TextInput, Alert } from "react-native";
 
 export default class MainView extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { showSuccessMessage: false};
+    }
+    
     render() {
         var name = this.props.navigation.state.params.loginName ;
+        var successMessage = this.state.showSuccessMessage ? 'Thank you, your answer has been submitted!' : '';
+        
         return (
                 <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                    <View style={{flex: 0.3}} />
+                    <View style={{flex: 0.2}} />
                     <Text style={{margin: 10}}>Hello {name}, please enter your last name</Text>
-                    <TextInput style={{width: 200, height: 30, backgroundColor: 'powderblue'}} />
-                    <View style={{flex: 0.5}} />
+                    <TextInput style={{width: 200, height: 30, backgroundColor: 'powderblue'}} />                    
+                    <Button title="Submit" onPress={() => {
+                            this.setState(previousState => {
+                                return { showSuccessMessage: !previousState.showSuccessMessage };
+                        }); }
+                    }/>
+                    <Text>{successMessage}</Text>
+                    <View style={{flex: 0.2}} />
                 </View>
         );
     }
